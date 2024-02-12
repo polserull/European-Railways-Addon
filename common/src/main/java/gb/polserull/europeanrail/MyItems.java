eadaa1b0ad2771c724cfc234320c1b8aafccf3bf
polserull <fatherofno@gmail.com>
2024-02-12 17:13:09 +0000

    Fixes
    Removed unused Creative Tabs

--- common/src/main/java/gb/polserull/europeanrail/MyItems.java
+++ common/src/main/java/gb/polserull/europeanrail/MyItems.java
@@ -8,8 +8,8 @@ import static gb.polserull.europeanrail.Main.MOD_ID;
 
 public interface MyItems {
     //RegistryObject<Item> ADVANCE_TICKET = new RegistryObject<>(() -> new advanceTicket(new Item.Properties()));
-    CreativeModeTabs.Wrapper EUROPEAN_RAILWAYS = new CreativeModeTabs.Wrapper(new ResourceLocation(MOD_ID, "europe_railways_menu"), () -> new ItemStack(MyBlocks.ADVANCED_CROSSING_SIGN.get()));
+    //CreativeModeTabs.Wrapper EUROPEAN_RAILWAYS = new CreativeModeTabs.Wrapper(new ResourceLocation(MOD_ID, "europe_railways_menu"), () -> new ItemStack(MyBlocks.ADVANCED_CROSSING_SIGN.get()));
     CreativeModeTabs.Wrapper UK_RAILWAYS = new CreativeModeTabs.Wrapper(new ResourceLocation(MOD_ID, "uk_railways_menu"), () -> new ItemStack(MyBlocks.ADVANCED_CROSSING_SIGN.get()));
-    CreativeModeTabs.Wrapper DUTCH_RAILWAYS = new CreativeModeTabs.Wrapper(new ResourceLocation(MOD_ID, "dutch_railways_menu"), () -> new ItemStack(MyBlocks.ADVANCED_CROSSING_SIGN.get()));
+    //CreativeModeTabs.Wrapper DUTCH_RAILWAYS = new CreativeModeTabs.Wrapper(new ResourceLocation(MOD_ID, "dutch_railways_menu"), () -> new ItemStack(MyBlocks.ADVANCED_CROSSING_SIGN.get()));
 }
 
